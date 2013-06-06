67f686d43e7eb19273efe3af66d289e005bf6e71
Dawars <dawars00@gmail.com>
2013-06-06 23:32:12 +0200

    Bats and Squids should be banned FINALLY!

--- mcp/src/minecraft/mods/elysium/handlers/ElysianCreatureSpawnHandler.java
+++ mcp/src/minecraft/mods/elysium/handlers/ElysianCreatureSpawnHandler.java
@@ -2,6 +2,7 @@ package mods.elysium.handlers;
 
 import mods.elysium.Elysium;
 import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.passive.EntityBat;
 import net.minecraft.entity.passive.EntitySquid;
 import net.minecraft.world.World;
 import net.minecraftforge.event.ForgeSubscribe;
@@ -14,7 +15,7 @@ public class ElysianCreatureSpawnHandler
     public void CheckSpawn(LivingSpawnEvent event)
     {
        	if(event.world.provider.dimensionId == Elysium.DimensionID){
-       		if(event.entity instanceof EntitySquid){
+       		if(event.entity instanceof EntitySquid || event.entity instanceof EntityBat){
 				event.setResult(Result.DENY);
        		}
        	}
