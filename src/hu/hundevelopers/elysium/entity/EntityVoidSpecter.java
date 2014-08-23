9db34ea7f54a7a3dd344d8387eebe416d1f3185b
Dawars <dawars00@gmail.com>
2014-08-23 22:53:49 +0200

    Experimenting with VoidSpecter1

--- src/hu/hundevelopers/elysium/entity/EntityVoidSpecter.java
+++ src/hu/hundevelopers/elysium/entity/EntityVoidSpecter.java
@@ -1,46 +1,29 @@
 package hu.hundevelopers.elysium.entity;
 
-import java.util.Iterator;
-import java.util.List;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockEndPortal;
-import net.minecraft.block.material.Material;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.IEntityMultiPart;
 import net.minecraft.entity.SharedMonsterAttributes;
-import net.minecraft.entity.boss.EntityDragonPart;
-import net.minecraft.entity.item.EntityEnderCrystal;
-import net.minecraft.entity.item.EntityXPOrb;
-import net.minecraft.entity.monster.IMob;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Blocks;
-import net.minecraft.util.AxisAlignedBB;
-import net.minecraft.util.DamageSource;
 import net.minecraft.util.MathHelper;
 import net.minecraft.util.Vec3;
-import net.minecraft.world.Explosion;
 import net.minecraft.world.World;
 
-public class EntityVoidSpecter extends EntityLiving implements IMob
+public class EntityVoidSpecter extends EntityLiving
 {
     public double targetX;
     public double targetY;
     public double targetZ;
     private Entity target;
-    public int deathTicks;
-    /** The current endercrystal that is healing this dragon */
-    public EntityEnderCrystal healingEnderCrystal;
-
+    
     public EntityVoidSpecter(World par1World)
     {
         super(par1World);
-        this.setHealth(this.getMaxHealth());
         this.setSize(2.0F, 2.0F);
-        this.noClip = false;
         this.isImmuneToFire = true;
         this.targetY = 100.0D;
+        this.targetX = this.posX + this.rand.nextGaussian() * 2.0F;
+        this.targetZ = this.posZ + this.rand.nextGaussian() * 2.0F;
+        this.noClip = true;
+
     }
 
     @Override
@@ -51,57 +34,97 @@ public class EntityVoidSpecter extends EntityLiving implements IMob
     }
 
     /**
+     * returns if this entity triggers Block.onEntityWalking on the blocks they walk on. used for spiders and wolves to
+     * prevent them from trampling crops
+     */
+    @Override
+    protected boolean canTriggerWalking()
+    {
+        return false;
+    }
+    
+    /**
+     * Return whether this entity should NOT trigger a pressure plate or a tripwire.
+     */
+    @Override
+    public boolean doesEntityNotTriggerPressurePlate()
+    {
+        return true;
+    }
+    /**
+     * Sets a new target for the flight AI. It can be a random coordinate or a nearby player.
+     */
+    private void setNewTarget()
+    {
+        if (this.rand.nextInt(2) == 0 && !this.worldObj.playerEntities.isEmpty())//TODO: add heroes
+        {
+            this.target = (Entity)this.worldObj.playerEntities.get(this.rand.nextInt(this.worldObj.playerEntities.size()));
+        }
+        else
+        {
+            this.targetX = this.posX + this.rand.nextInt(20) - 1;
+            this.targetZ = this.posZ + this.rand.nextInt(20) - 1;
+            this.targetY = this.worldObj.getTopSolidOrLiquidBlock((int) this.targetX, (int) this.targetZ) + 20.0F + this.rand.nextInt(10);
+//            double dx = this.posX - this.targetX;
+//            double dy = this.posY - this.targetY;
+//            double dz = this.posZ - this.targetZ;
+
+            this.target = null;
+        }
+    }
+    
+    /**
+     * Basic mob attack. Default to touch of death in EntityCreature. Overridden by each mob to define their attack.
+     */
+    protected void attackEntity(Entity par1Entity, float par2)
+    {
+        if (this.attackTime <= 0 && par2 < 1.2F && par1Entity.boundingBox.maxY > this.boundingBox.minY && par1Entity.boundingBox.minY < this.boundingBox.maxY)
+        {
+            this.attackTime = 20;
+            this.attackEntityAsMob(par1Entity);
+        }
+    }
+    
+    /**
      * Called frequently so the entity can update its state every tick as required. For example, zombies and skeletons
      * use this to react to sunlight and start to burn.
      */
-    @Override
     public void onLivingUpdate()
     {
-        float f;
-        float f1;
-
-    	this.updateDragonEnderCrystal();
-        f = 0.2F / (MathHelper.sqrt_double(this.motionX * this.motionX + this.motionZ * this.motionZ) * 10.0F + 1.0F);
-        f *= (float)Math.pow(2.0D, this.motionY);
-
-
-        this.rotationYaw = MathHelper.wrapAngleTo180_float(this.rotationYaw);
-
-        double d0;
-        double d1;
-        double distSq;
-        double d10;
-        float f12;
-
-        if (this.worldObj.isRemote)
+    	double dx;
+    	double dy;
+    	double dz;
+    	double dist;
+         
+    	if (this.worldObj.isRemote)
         {
             if (this.newPosRotationIncrements > 0)
             {
-                d10 = this.posX + (this.newPosX - this.posX) / (double)this.newPosRotationIncrements;
-                d0 = this.posY + (this.newPosY - this.posY) / (double)this.newPosRotationIncrements;
-                d1 = this.posZ + (this.newPosZ - this.posZ) / (double)this.newPosRotationIncrements;
-                distSq = MathHelper.wrapAngleTo180_double(this.newRotationYaw - (double)this.rotationYaw);
-                this.rotationYaw = (float)((double)this.rotationYaw + distSq / (double)this.newPosRotationIncrements);
+                dx = this.posX + (this.newPosX - this.posX) / (double)this.newPosRotationIncrements;
+                dy = this.posY + (this.newPosY - this.posY) / (double)this.newPosRotationIncrements;
+                dz = this.posZ + (this.newPosZ - this.posZ) / (double)this.newPosRotationIncrements;
+                dist = MathHelper.wrapAngleTo180_double(this.newRotationYaw - (double)this.rotationYaw);
+                this.rotationYaw = (float)((double)this.rotationYaw + dist / (double)this.newPosRotationIncrements);
                 this.rotationPitch = (float)((double)this.rotationPitch + (this.newRotationPitch - (double)this.rotationPitch) / (double)this.newPosRotationIncrements);
                 --this.newPosRotationIncrements;
-                this.setPosition(d10, d0, d1);
+                this.setPosition(dx, dy, dz);
                 this.setRotation(this.rotationYaw, this.rotationPitch);
             }
         }
         else
         {
-            d10 = this.targetX - this.posX;
-            d0 = this.targetY - this.posY;
-            d1 = this.targetZ - this.posZ;
-            distSq = d10 * d10 + d0 * d0 + d1 * d1;
+            dx = this.targetX - this.posX;
+            dy = this.targetY - this.posY;
+            dz = this.targetZ - this.posZ;
+            dist = dx * dx + dy * dy + dz * dz;
 
             if (this.target != null)
             {
                 this.targetX = this.target.posX;
                 this.targetZ = this.target.posZ;
-                double dx = this.targetX - this.posX;
-                double dz = this.targetZ - this.posZ;
-                double d7 = Math.sqrt(dx * dx + dz * dz);
+                double d3 = this.targetX - this.posX;
+                double d5 = this.targetZ - this.posZ;
+                double d7 = Math.sqrt(d3 * d3 + d5 * d5);
                 double d8 = 0.4000000059604645D + d7 / 80.0D - 1.0D;
 
                 if (d8 > 10.0D)
@@ -117,27 +140,28 @@ public class EntityVoidSpecter extends EntityLiving implements IMob
                 this.targetZ += this.rand.nextGaussian() * 2.0D;
             }
 
-            if (distSq < 100.0D || distSq > 22500.0D || this.isCollidedHorizontally || this.isCollidedVertically)
+            if (dist < 2 || dist > 16*16.0D || this.isCollidedHorizontally || this.isCollidedVertically)
             {
                 this.setNewTarget();
             }
+            
 
-            d0 /= (double)MathHelper.sqrt_double(d10 * d10 + d1 * d1);
-            f12 = 0.6F;
+            dx /= (double)MathHelper.sqrt_double(dx * dx + dy * dy);
+            float f12 = 0.6F;
 
-            if (d0 < (double)(-f12))
+            if (dy < (double)(-f12))
             {
-                d0 = (double)(-f12);
+                dy = (double)(-f12);
             }
 
-            if (d0 > (double)f12)
+            if (dy > (double)f12)
             {
-                d0 = (double)f12;
+                dy = (double)f12;
             }
 
-            this.motionY += d0 * 0.10000000149011612D;
+            this.motionY += dy * 0.10000000149011612D;
             this.rotationYaw = MathHelper.wrapAngleTo180_float(this.rotationYaw);
-            double d4 = 180.0D - Math.atan2(d10, d1) * 180.0D / Math.PI;
+            double d4 = 90.0D - Math.atan2(dx, dz) * 180.0D / Math.PI;
             double d6 = MathHelper.wrapAngleTo180_double(d4 - (double)this.rotationYaw);
 
             if (d6 > 50.0D)
@@ -186,146 +210,11 @@ public class EntityVoidSpecter extends EntityLiving implements IMob
     }
 
     /**
-     * Updates the state of the enderdragon's current endercrystal.
-     */
-    private void updateDragonEnderCrystal()
-    {
-        if (this.healingEnderCrystal != null)
-        {
-            if (this.healingEnderCrystal.isDead)
-            {
-                if (!this.worldObj.isRemote)
-                {
-//                    this.attackEntityFromPart(this.dragonPartHead, DamageSource.setExplosionSource((Explosion)null), 10.0F);
-                }
-
-                this.healingEnderCrystal = null;
-            }
-            else if (this.ticksExisted % 10 == 0 && this.getHealth() < this.getMaxHealth())
-            {
-                this.setHealth(this.getHealth() + 1.0F);
-            }
-        }
-
-        if (this.rand.nextInt(10) == 0)
-        {
-            float f = 32.0F;
-            List list = this.worldObj.getEntitiesWithinAABB(EntityEnderCrystal.class, this.boundingBox.expand((double)f, (double)f, (double)f));
-            EntityEnderCrystal entityendercrystal = null;
-            double d0 = Double.MAX_VALUE;
-            Iterator iterator = list.iterator();
-
-            while (iterator.hasNext())
-            {
-                EntityEnderCrystal entityendercrystal1 = (EntityEnderCrystal)iterator.next();
-                double d1 = entityendercrystal1.getDistanceSqToEntity(this);
-
-                if (d1 < d0)
-                {
-                    d0 = d1;
-                    entityendercrystal = entityendercrystal1;
-                }
-            }
-
-            this.healingEnderCrystal = entityendercrystal;
-        }
-    }
-
-
-    /**
-     * Sets a new target for the flight AI. It can be a random coordinate or a nearby player.
-     */
-    private void setNewTarget()
-    {
-        if (this.rand.nextInt(2) == 0 && !this.worldObj.playerEntities.isEmpty())
-        {
-            this.target = (Entity)this.worldObj.playerEntities.get(this.rand.nextInt(this.worldObj.playerEntities.size()));
-        }
-        else
-        {
-            boolean flag = false;
-
-            do
-            {
-                this.targetX = 0.0D;
-                this.targetY = (double)(70.0F + this.rand.nextFloat() * 50.0F);
-                this.targetZ = 0.0D;
-                this.targetX += (double)(this.rand.nextFloat() * 120.0F - 60.0F);
-                this.targetZ += (double)(this.rand.nextFloat() * 120.0F - 60.0F);
-                double d0 = this.posX - this.targetX;
-                double d1 = this.posY - this.targetY;
-                double d2 = this.posZ - this.targetZ;
-                flag = d0 * d0 + d1 * d1 + d2 * d2 > 100.0D;
-            }
-            while (!flag);
-
-            this.target = null;
-        }
-    }
-
-    /**
      * Simplifies the value of a number by adding/subtracting 180 to the point that the number is between -180 and 180.
      */
     private float simplifyAngle(double par1)
     {
         return (float)MathHelper.wrapAngleTo180_double(par1);
-    }
-
-    /**
-     * Creates the ender portal leading back to the normal world after defeating the enderdragon.
-     */
-    private void createEnderPortal(int par1, int par2)
-    {
-        byte b0 = 64;
-        BlockEndPortal.field_149948_a = true;
-        byte b1 = 4;
-
-        for (int k = b0 - 1; k <= b0 + 32; ++k)
-        {
-            for (int l = par1 - b1; l <= par1 + b1; ++l)
-            {
-                for (int i1 = par2 - b1; i1 <= par2 + b1; ++i1)
-                {
-                    double d0 = (double)(l - par1);
-                    double d1 = (double)(i1 - par2);
-                    double d2 = d0 * d0 + d1 * d1;
-
-                    if (d2 <= ((double)b1 - 0.5D) * ((double)b1 - 0.5D))
-                    {
-                        if (k < b0)
-                        {
-                            if (d2 <= ((double)(b1 - 1) - 0.5D) * ((double)(b1 - 1) - 0.5D))
-                            {
-                                this.worldObj.setBlock(l, k, i1, Blocks.bedrock);
-                            }
-                        }
-                        else if (k > b0)
-                        {
-                            this.worldObj.setBlock(l, k, i1, Blocks.air);
-                        }
-                        else if (d2 > ((double)(b1 - 1) - 0.5D) * ((double)(b1 - 1) - 0.5D))
-                        {
-                            this.worldObj.setBlock(l, k, i1, Blocks.bedrock);
-                        }
-                        else
-                        {
-                            this.worldObj.setBlock(l, k, i1, Blocks.end_portal);
-                        }
-                    }
-                }
-            }
-        }
-
-        this.worldObj.setBlock(par1, b0 + 0, par2, Blocks.bedrock);
-        this.worldObj.setBlock(par1, b0 + 1, par2, Blocks.bedrock);
-        this.worldObj.setBlock(par1, b0 + 2, par2, Blocks.bedrock);
-        this.worldObj.setBlock(par1 - 1, b0 + 2, par2, Blocks.torch);
-        this.worldObj.setBlock(par1 + 1, b0 + 2, par2, Blocks.torch);
-        this.worldObj.setBlock(par1, b0 + 2, par2 - 1, Blocks.torch);
-        this.worldObj.setBlock(par1, b0 + 2, par2 + 1, Blocks.torch);
-        this.worldObj.setBlock(par1, b0 + 3, par2, Blocks.bedrock);
-        this.worldObj.setBlock(par1, b0 + 4, par2, Blocks.dragon_egg);
-        BlockEndPortal.field_149948_a = false;
     }
 
 
--- src/hu/hundevelopers/elysium/model/ModelVoidSpecter.java
+++ src/hu/hundevelopers/elysium/model/ModelVoidSpecter.java
@@ -225,8 +225,6 @@ public class ModelVoidSpecter extends ModelBase
 
 	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5)
 	{
-		super.render(entity, f, f1, f2, f3, f4, f5);
-		setRotationAngles(entity, f, f1, f2, f3, f4, f5);
 		leg2R.render(f5);
 		leg1R.render(f5);
 		leg1L.render(f5);
@@ -261,11 +259,6 @@ public class ModelVoidSpecter extends ModelBase
 		model.rotateAngleX = x;
 		model.rotateAngleY = y;
 		model.rotateAngleZ = z;
-	}
-
-	public void setRotationAngles(Entity entity, float f, float f1, float f2, float f3, float f4, float f5)
-	{
-		super.setRotationAngles(f, f1, f2, f3, f4, f5, entity);
 	}
 
 }
